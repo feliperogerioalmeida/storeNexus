b31773187098c061adedada6937ac5001bfbcf4a
Felipe Rogerio <felipe.rogerio@outlook.com>
2024-11-25 07:58:27 -0300

    feat: add page to show category products

--- app/catalog/components/category-item.tsx
+++ app/catalog/components/category-item.tsx
@@ -1,5 +1,6 @@
 import { Category } from "@prisma/client";
 import Image from "next/image";
+import Link from "next/link";
 
 interface CategoryItemProps{
     category: Category
@@ -7,7 +8,8 @@ interface CategoryItemProps{
 
 const CategoryItem = ({category} : CategoryItemProps) => {
     return (  
-        <div className="flex flex-col ">
+        <Link href={`/category/${category.slug}`}>
+            <div className="flex flex-col ">
             <div className=" bg-category-item-gradient rounded-tl-lg rounded-tr-lg w-full h-[150px] items-center justify-center flex">
                 <Image
                     src={category.imageUrl}
@@ -26,6 +28,7 @@ const CategoryItem = ({category} : CategoryItemProps) => {
                 <p className="text-center font-semibold text-sm">{category.name}</p>
             </div>
         </div>     
+        </Link>
     );
 }    
 export default CategoryItem;
---
+++ app/category/[slug]/page.tsx
@@ -0,0 +1,38 @@
+import { Badge } from "@/app/_components/ui/badge";
+import ProductItem from "@/app/_components/ui/product-item";
+import { db } from "@/app/_lib/prisma";
+import { CATEGORY_ICON } from "@/app/constants/category-icon";
+import { computeProductTotalPrice } from "@/app/product";
+
+
+const CategoryProducts = async ({params}: any) => {
+    
+    const category = await db.category.findFirst({
+        where:{
+            slug: params.slug
+        },
+        include:{
+            products: true
+        }
+    });
+
+    if (!category) {
+        return null;
+    }
+    
+    return (  
+        <div className="flex flex-col gap-8 p-5">
+            <Badge className="w-fit gap-1 text-base uppercase border-primary px-3 py-[0.375rem] border-2" variant="outline">
+                    {CATEGORY_ICON[params.slug as keyof typeof CATEGORY_ICON]}
+                    {category.name}
+            </Badge>
+
+            <div className="grid grid-cols-2 gap-8">
+                {category.products.map(product => <ProductItem key={product.id} product = {computeProductTotalPrice(product)}/>)}
+            </div>
+
+        </div>
+    );
+}
+ 
+export default CategoryProducts;
