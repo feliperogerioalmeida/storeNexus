3461e8c63c75c69f2abe9dcc0ce751b0a2f0e65e
Felipe Rogerio <felipe.rogerio@outlook.com>
2024-11-24 21:58:15 -0300

    chore: add category-item gradient to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,8 @@ const config: Config = {
   	extend: {
   		backgroundImage: {
   			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-  			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+  			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			'category-item-gradient' : 'linear-gradient(45deg, #5033c3 0%, rgba(80,51,195,0.20) 100%) '
   		},
   		borderRadius: {
   			lg: 'var(--radius)',
