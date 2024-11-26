e122666fd97b950c40834b652b39cb21a0b31b15
Felipe Rogerio <felipe.rogerio@outlook.com>
2024-11-25 21:09:05 -0300

    feat: add CartItem Components

---
+++ app/_components/ui/cart-item.tsx
@@ -0,0 +1,63 @@
+import { CartProduct } from "@/app/_providers/cart";
+import { ArrowLeftIcon, ArrowRightIcon, TrashIcon } from "lucide-react";
+import Image from "next/image";
+import { Button } from "./button";
+
+interface CartItemProps{
+    product: CartProduct
+}
+
+const CartItem = ({product}: CartItemProps) => {
+    return ( 
+        <div className="flex items-center justify-between">
+            <div className=" flex items-center gap-4">
+                <div className="bg-accent flex items-center justify-center rounded-lg h-[77px] w-[77px]">
+                    <Image
+                        src={product.imageUrls[0]}
+                        alt={product.name}
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                        className="w-auto h-auto max-w-[80%] max-h-[70%]"
+                    />
+                </div>
+
+                <div className="flex flex-col gap-1">
+                    <p className="text-xs">{product.name}</p>
+
+                    <div className="flex items-center gap-2"> 
+                        <p className="text-sm font-bold">
+                            R$ {product.totalPrice.toFixed(2)}
+                        </p>
+
+                        {product.discountPercentage > 0 &&(
+                            <p className="opacity-75 text-xs line-through"> 
+                                {Number(product.basePrice).toFixed(2)}
+                            </p>
+                        )}
+                    </div>
+
+                    <div className="flex items-center gap-1">
+                        <Button size="icon" variant="outline" className="h-8 w-8">
+                            <ArrowLeftIcon size={16} className="h-4 w-4" />
+                        </Button>
+
+                        <span className="text-xs"> {product.quantity} </span>
+                        
+                        <Button size="icon" variant="outline" className="h-8 w-8">
+                            <ArrowRightIcon size={16} className="h-4 w-4"/>
+                        </Button>
+                    </div>
+                </div>
+            </div>
+
+            <Button size="icon" variant="outline" className="h-8 w-8">
+                <TrashIcon size={16} className="h-4 w-4"/>
+            </Button>
+
+        </div>
+
+    );
+}
+ 
+export default CartItem;
--- app/_providers/cart.tsx
+++ app/_providers/cart.tsx
@@ -1,9 +1,10 @@
 "use client"
 
-import { Product } from "@prisma/client";
-import { createContext, ReactNode, useState } from "react";
 
-interface CartProduct extends Product{
+import { createContext, ReactNode, useState } from "react";
+import { ProductWithTotalPrice } from "../helpers/product";
+
+export interface CartProduct extends ProductWithTotalPrice {
     quantity: number
 }
 
@@ -12,7 +13,7 @@ interface ICartContext {
     cartTotalPrice: number;
     cartBasePrice: number;
     cartTotalDiscount: number;
-    addProductToCart:(product: CartProduct) => void;
+    addProductToCart: (product: CartProduct) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
@@ -28,6 +29,31 @@ const CartProvider = ({children} : {children : ReactNode}) => {
     const [products, setProducts] = useState<CartProduct[]>([])
 
     const addProductToCart = (product: CartProduct) => {
+
+        // se o produto já estiver no carrinho, apenas aumente sua quantidade
+        const productsIsAlreadyOnCart = products.some(
+            (cartProduct) => cartProduct.id == product.id
+        );
+
+        if (productsIsAlreadyOnCart) {
+            setProducts((prev) => 
+            prev.map((cartProduct) => {
+                if (cartProduct.id == product.id) {
+                    return{
+                        ...cartProduct,
+                        quantity: cartProduct.quantity + 1
+                    };
+                }
+
+                return cartProduct;
+            }),
+        );
+
+        return;
+
+        }
+
+        // se não, adicione o produto a lista
         setProducts((prev) => [...prev, product])
     }
 
