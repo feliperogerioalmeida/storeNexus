94db4090518b4941f63f0ab0f48816b8dc2bc9f5
Felipe Rogerio <felipe.rogerio@outlook.com>
2024-12-03 15:59:37 -0300

    feat: add first desktop banner to home page

--- app/(home)/components/promo-banner.tsx
+++ app/(home)/components/promo-banner.tsx
@@ -1,11 +1,12 @@
+import { cn } from "@/app/_lib/utils";
 import Image, {ImageProps } from "next/image"
 
-const PromoBanner = ({alt, ...props}: ImageProps) => {
+const PromoBanner = ({alt, className, ...props}: ImageProps) => {
     return (  
         <Image
             width={0}
             height={0}
-            className="h-auto w-full px-5"
+            className={cn("h-auto w-full px-5", className)}
             sizes="100vw"
             alt={alt}
             {...props}
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,6 +3,7 @@ import { db } from "../_lib/prisma";
 import ProductList from "../_components/ui/product-list";
 import SectionTitle from "../_components/ui/section-title";
 import PromoBanner from "./components/promo-banner";
+import Image from "next/image";
 
 
 export default async function Home() {
@@ -31,46 +32,59 @@ export default async function Home() {
   })
  
   return (
-    <div className="flex flex-col gap-8 py-8">
-
-      <PromoBanner
-        src= "/banner-home-01.png"
-        alt="Até 55% de desconto esse mês!"
-      />
-
-      <div className=" px-5">
-        <Categories/>
-      </div>
-
-      <div>
-        <SectionTitle> ofertas </SectionTitle>
-
-        <ProductList products={deals}/>
-      </div>
-
-      <PromoBanner
-        src= "/banner-home-02.png"
-        alt="Até 55% de desconto em mouses!"
-      />
-
-      <div>
-        <SectionTitle> teclados </SectionTitle>
-
-        <ProductList products={keyboards}/>
-      </div>
-
+      <>
+        <div className="mx-auto max-w-[1920px]">
+          <Image
+            src="/deals-banner.png"
+            className="hidden h-auto w-full lg:block"
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt="Até 55% de desconto esse mês"
+          />
+          
+        </div>
       
-      <PromoBanner
-        src= "/banner-home-03.png"
-        alt="Até 55% de desconto em fones!"
-      />
+        <div className="container mx-auto flex flex-col gap-8 py-8">
+          <PromoBanner
+            src= "/banner-home-01.png"
+            alt="Até 55% de desconto esse mês!"
+            className="lg:hidden"
+          />
+        </div>
 
-      <div>
-        <SectionTitle> mouses </SectionTitle>
+        <div className=" px-5">
+          <Categories/>
+        </div>
 
-        <ProductList products={mouses}/>
-      </div>
+        <div>
+          <SectionTitle> ofertas </SectionTitle>
+          <ProductList products={deals}/>
+        </div>
 
-    </div>
+        <PromoBanner
+          src= "/banner-home-02.png"
+          alt="Até 55% de desconto em mouses!"
+        />
+
+        <div>
+          <SectionTitle> teclados </SectionTitle>
+          <ProductList products={keyboards}/>
+        </div>
+
+        <div>
+          <PromoBanner
+            src= "/banner-home-03.png"
+            alt="Até 55% de desconto em fones!"
+          />
+        </div>
+      
+
+        <div>
+          <SectionTitle> mouses </SectionTitle>
+          <ProductList products={mouses}/>
+        </div>
+      </>
+
   );
 }
