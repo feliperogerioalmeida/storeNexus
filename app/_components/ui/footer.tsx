a1e240044893a0bbcc5efca89c7cd4438c869fb3
Felipe Rogerio <felipe.rogerio@outlook.com>
2024-12-03 16:49:31 -0300

    feat: ensure footer is responsive on desktop

--- app/_components/ui/footer.tsx
+++ app/_components/ui/footer.tsx
@@ -1,7 +1,9 @@
 const Footer = () => {
     return (  
-       <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75">
-        © 2024 Copyright <span className="font-semibold">Orbi Store</span>
+        <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75 lg:text-sm">
+            <div className="mx-auto lg:container">
+                © 2024 Copyright <span className="font-semibold">Orbi Store</span>
+            </div>
        </div> 
     );
 }
