ce3330be1348f5c5ee1e0b80fbc630c63a224492
Felipe Almeida <felipe.rogerio@outlook.com>
2024-12-11 10:37:13 -0300

    feat: add LoadingButton component

---
+++ app/_components/ui/loading-button.tsx
@@ -0,0 +1,36 @@
+import { cn } from "@/app/_lib/utils";
+import { Button, ButtonProps } from "./button";
+import { Loader2 } from "lucide-react";
+
+interface LoadingButtonProps extends ButtonProps {
+    loading: boolean;
+    textWaiting: string;
+}
+
+
+const LoadingButton = ({
+    loading,
+    textWaiting,
+    className,
+    children,
+    ...props
+} : LoadingButtonProps) => {
+    return (
+        <Button
+            disabled={loading}
+            className={cn("flex gap-2 font-bold", className)}
+            {...props}
+        >
+            {loading ? (
+                <>
+                    <Loader2 className="w-4 animate-spin"/>
+                    {textWaiting}
+                </>
+            ): (
+                children
+            )}
+        </Button>
+      );
+}
+ 
+export default LoadingButton;
