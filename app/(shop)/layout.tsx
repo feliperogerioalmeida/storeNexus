f92cd4ebf5ded23f3d0f9a1d86f2324c6a89ae00
Felipe Almeida <felipe.rogerio@outlook.com>
2024-12-16 10:23:33 -0300

    fix: fix deploy error because eslint

--- app/(shop)/layout.tsx
+++ app/(shop)/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
             <CartProvider>
               <Header />
               <div className="flex-1">{children}</div>
-              <Toaster/>
+              <Toaster />
               <Footer />
             </CartProvider>
           </AuthProvider>
